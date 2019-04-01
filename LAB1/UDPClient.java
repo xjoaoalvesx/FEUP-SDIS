84cf181cb2bffe7b748cfee3270ee886a904de1b
Joao Alves <joao-carlos.alves@hotmail.com>
2019-04-01 15:01:23 +0100

    lab1

--- LAB1/UDPClient.java
+++ LAB1/UDPClient.java
@@ -9,25 +9,51 @@ class Client
 	public static void main(String[] args) throws IOException 
 	{
 
-		String host_name = args[0];
+        if(args.length < 4) {
+            System.out.println("Usage: java Client <hostname> <port_number> <oper> <opnd>*");
+            return;
+        }
+
+        InetAddress address = InetAddress.getByName(args[0]);
 		int port = Integer.parseInt(args[1]);
-		String operation = args[2];
+		String request = args[2];
+        String plate = args[3];
+
+        if(!plate.matches("\\w{2}-\\w{2}-\\w{2}")){
+            System.out.println("The plate format must be XX-XX-XX");
+        }
+
+        String msg = new String();
+
+        switch(request){
+            case "REGISTER":
+                if(args[4].length() > 256){
+                    System.out.println("Owner's name must have less than 256 characters.");
+                }
+                msg = "REGISTER" + args[3] + " " + args[4];
+                break;
+            case "LOOKUP":
+                msg = "LOOKUP" + args[3];
+                break;
+        }
 
 
+        DatagramSocket socket = sendMessage(msg, addres, port);
+        String response = receiveMessage(socket, addres, port);
 
+        System.out.println("Echoed Message:" + response);
+        socket.close();
 
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-      	DatagramSocket clientSocket = new DatagramSocket();
+        System.out.println("Client terminated!");
 
       	byte[] sendData = new byte[256];
     	byte[] receiveData = new byte[256];
 
-    	InetAddress IPAddress = InetAddress.getByName("localhost");
 
     	
-    	String msg = operation + " " + args[3];
+    	
     	sendData = msg.getBytes();
-    	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+    	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, port);
     	clientSocket.send(sendPacket);
     	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
     	clientSocket.receive(receivePacket);
@@ -37,6 +63,27 @@ class Client
 
 	}
 
+
+    private static DatagramSocket sendMessage(String message, InetAddress address, int port) throws IOException{
+        System.out.println(msg);
+        DatagramSocket socket = new DatagramSocket();
+        byte[] sbuf = msg.getBytes();
+
+        DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, port);
+        socket.send(packet);
+
+        return socket;
+    }
+
+    private static String receiveMessage()
+
+        byte[] rbuf = new byte[256];
+        DatagramPacket packet = new DatagramPacket(rbuf, rbuf.length);
+        socket.receive(packet);
+
+        String received = new String(packet.getData());
+
+        return received;
 	
 
 }
--- LAB1/UDPServer.java
+++ LAB1/UDPServer.java
@@ -1,8 +1,11 @@
 import java.net.DatagramPacket; 
 import java.net.DatagramSocket; 
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.io.*;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 class Server
@@ -10,56 +13,66 @@ class Server
 
 	public static void main(String[] args) throws IOException 
 	{
-		DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
-		byte[] receiveData = new byte[256];
-		byte[] sendData = new byte[256];
+
+
+              if (args.length != 1 && args.length != 2) {
+                     System.out.println("Usage: java Server <port_number> [<timeout>] ");
+                     return;
+              }
+
+              int port = Integer.parseInt(args[0]);
+              int timeout = args.length > 1 ? Integer.parseInt(args[1]) : 0;
+
+		DatagramSocket socket = new DatagramSocket(port);
+              socket.setSoTimeout(timeout * 1000);
+
+		byte[] receiveData = new byte[1024];
+              DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
+
+              System.out.println("Server has been initialized!");
+
+              Map<String, String> data = new HashMap<>();
 
 
 		while(true)
 		{
-       		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-       		serverSocket.receive(receivePacket);
+                     try{
+                            socket.receive(packet);
+                     }catch (SocketTimeoutException e) {
+                            System.out.println("Timeout");
+                            break;
+                     }
 
-       		String request = new String(receivePacket.getData());
+                     String msg = new String(packet.getData(), 0, packet.getLength());
+                     System.out.println("Received: " + msg.trim() + " from " + packet.getAddress() + ":" + packet.getPort());
 
-       		//indentifies the type of request 
-       		String[] msg = request.split(" ");
-       		String operation = msg[0].toUpperCase();
+                     String[] request = msg.split("\\s+");
+                     String response = new String();
 
-       		if(operation.equals("REGISTER"))
-       		{
-       			
-       		}
+                     switch(request[0]){
 
-       		else if(operation.equals("LOOKUP"))
-       		{
-       			
-       		}
+                            case "REGISTER":
 
-       		InetAddress IPAdd = receivePacket.getAddress();
-       		int port = receivePacket.getPort();
+                            if(! isValidPlate(request[1]) || database.containsKey(request[1])){
+                                   System.out.println("The plate was registered successfully");
+                                   response = "-1";
+                            }
+                            else{
+                                   database.put(request[1], request[2])
+                                   response = Integer.tostring(database.size());
+                            }
 
-       		sendData = operation.getBytes();
 
-       		DatagramPacket responsePacket = new DatagramPacket(sendData, sendData.length, IPAdd, port);
-       		serverSocket.send(responsePacket);
+                     }
+       		
 
 
 		}
 	}
 
 
-	public static void register()
-	{
-
-	}
-
-
-	public static void lookup()
-	{
-
-	}
+	
 
 
 
