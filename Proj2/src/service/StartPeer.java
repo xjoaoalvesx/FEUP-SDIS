406c91eead0b4fcf0377d80fda7d8b8ad8cf5143
João Ferreira <up201605330@g.uporto.pt>
2019-05-20 15:44:12 +0100

    StartPeer can initialize a Peer

--- Proj2/src/service/StartPeer.java
+++ Proj2/src/service/StartPeer.java
@@ -9,14 +9,21 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
+
 public class StartPeer {
 
-	
 	public static void main(String[] args){
 
+		if(args.length != 1){
+			System.out.println("Usage:\njava -classpath bin service.StartPeer <peer_id>");
+			return;
+		}
+
+		int peer_id = Integer.parseInt(args[0]);
+
+		Peer peer = new Peer(peer_id);
+
+		
+		System.out.println("Peer " + String.valueOf(peer_id) + " has been created.");
 	}
-
-
-
-
 }
